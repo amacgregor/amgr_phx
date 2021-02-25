d789c406f59c719261d38bf3f3935ad9a573f145
Allan MacGregor <amacgregor@allanmacgregor.com>
2021-02-25 18:25:17 -0500

    Update for configuration for gigalir

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,19 @@ config :amgr, AmgrWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+
+config :amgr, AmgrWeb.Endpoint,
+       http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+       url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+       secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+       server: true
+
+config :amgr, AmgrWeb.Repo,
+       adapter: Ecto.Adapters.Postgres,
+       url: System.get_env("DATABASE_URL"),
+       ssl: true,
+       pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
