3b34b892efbc8e731e6dd8b169dc0c00394660f8
Allan MacGregor <amacgregor@allanmacgregor.com>
2021-02-26 14:51:50 -0500

    Kill Death code

--- lib/amgr_web/views/sitemap_view.ex
+++ lib/amgr_web/views/sitemap_view.ex
@@ -1,23 +1,5 @@
 defmodule AmgrWeb.SitemapView do
   use AmgrWeb, :view
-  alias AmgrWeb.SEO.Generic
-  @generic %Generic{}
-
-  def render("sitemap.xml", %{}) do
-    AmgrWeb.Rss.generate(%AmgrWeb.Rss{
-      title: @generic.title,
-      author: "Allan MacGregor",
-      description: @generic.description,
-      posts: Amgr.Blog.published_posts()
-    })
-  end
-
-  def format_date(date) do
-    date
-    |> DateTime.from_naive!("Etc/UTC")
-    |> DateTime.to_date()
-    |> to_string()
-  end
 
   def root_domain() do
     Application.get_env(:amgr, AmgrWeb.Endpoint)[:asset_url] || ""
