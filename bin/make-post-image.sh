df6099c1d7cd393fd8e057623c164165133631e7
Allan MacGregor <amacgregor@allanmacgregor.com>
2021-02-26 09:33:54 -0500

    Update for TIL image generation and make it more configurable

--- bin/make-post-image.sh
+++ bin/make-post-image.sh
@@ -4,10 +4,11 @@ set -eu -o pipefail
 
 ID=$1
 TITLE=$2
+BASE_IMAGE=$3 || "./assets/static/images/patterns/pattern-abstract-3.png"
 
 mkdir ./assets/static/images/cards &>/dev/null || true
 
-convert "./assets/static/images/patterns/pattern-abstract-3.png" \
+convert $BASE_IMAGE \
   \( -background none -fill white -size 320x -font ./assets/static/fonts/FiraCode-SemiBold.ttf \
      label:"allanmacgregor" \
   \) -gravity southeast -geometry +40+20 -compose over -composite \
