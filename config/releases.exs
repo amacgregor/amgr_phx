a16d3cd20b075251a61870eb0b5997871cd0193d
Allan MacGregor <amacgregor@allanmacgregor.com>
2021-02-25 20:05:23 -0500

    Fix for release config

--- config/releases.exs
+++ config/releases.exs
@@ -4,5 +4,5 @@ config :amgr, AmgrWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
   url: [host: "amgr.dev", port: 443],
-  asset_url: "https://allanmacgregor.com"
+  asset_url: "https://allanmacgregor.com",
   check_origin: ["https://amgr.dev", "//amgr.dev", "//allanmacgregor.com"]
