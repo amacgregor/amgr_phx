771cdf3322f8875e41efe023fd06f6c6e7e06afc
Allan MacGregor <amacgregor@allanmacgregor.com>
2022-05-19 17:30:47 -0300

    Fix checks

--- mix.exs
+++ mix.exs
@@ -66,7 +66,11 @@ defmodule Amgr.MixProject do
     [
       setup: ["deps.get", "cmd yarn --cwd ./assets install"],
       check: ["format --check-formatted", "sobelow -i Config.HTTPS", "credo"],
-      "assets.deploy": ["cmd 'npm install --prefix ./assets'", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": [
+        "cmd 'npm install --prefix ./assets'",
+        "esbuild default --minify",
+        "phx.digest"
+      ]
     ]
   end
 end
