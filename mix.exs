b77135b95df236f5426c355ae65c61f178ccbf20
Allan MacGregor <amacgregor@allanmacgregor.com>
2022-05-23 16:53:32 -0300

    Disable database for this app

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Amgr.MixProject do
   def project do
     [
       app: :amgr,
-      version: File.read!("VERSION") |> String.trim(),
+      version: "0.3.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
